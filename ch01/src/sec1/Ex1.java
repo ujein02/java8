72480028d6daaa578f25c637487bc66bad80bef3
ujein02 <ujein02@gmail.com>
2022-08-31 17:18:12 +0900

    java starting and webtest

---
+++ ch01/src/sec1/Ex1.java
@@ -0,0 +1,17 @@
+package sec1;
+
+//실행클래스
+public class Ex1 {
+
+	public static void main(String[] args) {
+		System.out.println("하이~! 자바~!");
+		Ex2 ex2 = new Ex2();
+		ex2.method1();
+		Ex3 st1 = new Ex3();
+		st1.setName("김지은");
+		st1.setKor(100);
+		st1.setMat(100);
+		st1.setEng(80);
+		System.out.println("st1의 학생의 이름은 "+st1.getName());
+	}
+}
