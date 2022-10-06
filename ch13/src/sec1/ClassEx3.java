6494ac070121eba9594db2d207c22fb99d7278dd
ujein02 <ujein02@gmail.com>
2022-10-06 16:18:44 +0900

    java generic and lambda

---
+++ ch13/src/sec1/ClassEx3.java
@@ -0,0 +1,35 @@
+package sec1;
+
+public class ClassEx3 {
+
+	public static void main(String[] args) {
+		Box<Integer> box1 = Util.<Integer>boxing(100);	//첫번째 방법
+		Box<String> box0 = Util.boxing("김기태");
+		int intValue = box1.get();
+		System.out.println(intValue);
+		String strValue0 = box0.get();
+		System.out.println(strValue0);
+		Box<String> box2 = Util.boxing("김기태");			// 두번째 방법
+		String strValue = box2.get();
+		//Box<
+
+	}
+
+}
+class Util{
+	public static <T> Box<T> boxing(T t){
+		Box<T> box = new Box<T>();
+		box.set(t);
+		return box;
+	}
+}
+
+class Box<T>{
+	private T t;
+
+	public T get() {return t;}
+
+	public void set(T t) {	this.t = t;	}
+	
+	
+}
